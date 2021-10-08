21830c2710e539a0a3c73bce19b1403cf59da4a1
hrifo <hetoralexanderrifo@gmail.com>
2021-10-07 22:23:15 -0300

    Primer commit

---
+++ 2_3_1_condiciones_anidadas.py
@@ -0,0 +1,23 @@
+# numero = 13
+# if numero>=0 and numero <=100: #primer
+#     if numero>=0 and numero<=10: #segundo
+#         print('Numero está entre 0 y 10')
+#     if numero>=11 and numero<=20:#tercero
+#         print('Numero está entre 11 y 20')
+
+vocal = input('Ingrese vocal: ')
+
+if vocal == "a":
+    print('La vocal es la a')
+elif vocal =="e":
+    print('La vocal es la e')
+elif vocal =="i":
+    print('La vocal es la i')
+elif vocal =="o":
+    print('La vocal es la o')
+elif vocal =="u":
+    print('La vocal es la u')
+elif vocal =="i":
+    print('La vocal es la i por segunda vez')
+else:
+    print('Lo ingresado por teclado no es una vocal')
